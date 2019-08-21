2481c454c7e4860b098b457115160a0d7b326f26
nyathiss <sazi.nyathi@lexisnexis.co.za>
2019-08-21 10:19:23 +0200

    Code Review Changes By Welcome

--- Program.cs
+++ Program.cs
@@ -8,18 +8,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            string sentence = "I love to with my dad and mom but they are far away me";
-            if (checkIfSentenceHasValue(sentence) != null)
+            string sentence = "I love to LIVE with my parent but there are to far away";
+            if (CheckIfSentenceHasValue(sentence) == null)
+                Console.WriteLine("Please add the sentence");
+            else
             {
-                var lowercase = LowerCaseAllWordInSentence(sentence);
-                var sentenceSplit = BreakSentenceBySpaces(lowercase);
-                sentenceSplit[0] = UpperCaseTheFirstLetter(sentenceSplit.First());
-                sentenceSplit[sentenceSplit.Length - 1] = UpperCaseTheFirstLetter(sentenceSplit.Last());
-                for (int i = 1; i < sentenceSplit.Length - 1; i++)
+                var sentenceSplit = sentence.ToLower().Split(' ');
+                    
+                for (int i = 0; i < sentenceSplit.Length; i++)
                 {
-                    if (lowerList.Contains(sentenceSplit[i]))
+                    if (DefaultRule().Contains(sentenceSplit[i]))
                     {
-                        sentenceSplit[i] = LowerCaseAllWordInSentence(sentenceSplit[i]);
+                        sentenceSplit[i] = sentenceSplit[i];
                     }
                     else
                     {
@@ -29,35 +29,25 @@ namespace ConsoleApp2
                 string results = string.Join(" ", sentenceSplit);
                 Console.WriteLine(results);
             }
-            else
-            {
-                Console.WriteLine("Please add the sentence");
 
-            }
             Console.ReadLine();
         }
-        private static List<string> lowerList = new List<string> { "the", "to", "in", "with", "and", "but", "or" };
+      
         private static string UpperCaseTheFirstLetter(string word)
         {
             return word.Trim().Substring(0, 1).ToUpper() + word.Trim().Substring(1);
-
         }
 
-        private static string LowerCaseAllWordInSentence(string sentence)
+        private static List<string> DefaultRule()
         {
-            return sentence.ToLower();
+            return new List<string> { "the", "to", "in", "with", "and", "but", "or" };
         }
-
-        private static string checkIfSentenceHasValue(string sentence)
+        private static string CheckIfSentenceHasValue(string sentence)
         {
             if (string.IsNullOrEmpty(sentence))
                 return null;
             return sentence;
         }
 
-        private static string[] BreakSentenceBySpaces(string sentence)
-        {
-            return sentence.Split(' ');
-        }
     }
 }
